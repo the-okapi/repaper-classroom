5847e95f7ee5def04bb4d1468f57a65ed33c6609
Theo Kapi <theo_kapi@outlook.com>
2026-06-25 17:04:34 -0700

    Updating some more stuff

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Popover } from 'bits-ui';
+    import Book from '$lib/components/Book.svelte';
+</script>
+
+<Popover.Root>
+    <Popover.Trigger class="absolute top-5 left-5 p-1.5 border border-(--o) rounded-lg cursor-pointer">
+        <Book class="size-6 fill-(--p)" />
+    </Popover.Trigger>
+</Popover.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import './layout.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import favicon from '$lib/assets/favicon.png';
+	import NavBar from '$lib/components/NavBar.svelte';
 
 	let { children } = $props();
 </script>
 
-<ModeWatcher darkClassNames={["dark"]} lightClassNames={["light"]} />
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher darkClassNames={['dark']} lightClassNames={['light']} />
+
+<NavBar />
+
 {@render children()}
